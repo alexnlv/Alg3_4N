048df7765f89d9dde751a25db93eeda411b042f7
user <user@192.168.254.2>
2013-09-28 03:50:28 -0300

    entradas do usuario ok...

--- Trabalho_2/src/Principal.java
+++ Trabalho_2/src/Principal.java
@@ -11,163 +11,234 @@ public class Principal {
 		
 		while(!cmd.equals("fim")){
 			
-			cmd = scan.nextLine();
+			String[] parametros = scan.nextLine().split(" ");
+			cmd = parametros[0].trim();
 			
-			if(cmd.startsWith("imagem")){
+			if ((parametros.length < 2)){
 				
-				criarImagem(cmd);
+				if(cmd.equals("fim"));
+				else{
+					System.out.println("Paramentros incorretos");
+					}
 				
-			}
+			} else {
+				
+				if(testarParametros(parametros)){
+											
+						executaParametros(parametros);
+					
+						
+					} else if(!cmd.equals("fim")){
+						System.out.println("Paramentros incorretos");
+					}else{
+						System.out.println("Paramentros incorretos");
+					}
+			
+			}	
+		}
+	}
+
+	private static boolean testarParametros(String[] parametros) {
 		
-			if(cmd.startsWith("salvar")){
+		boolean resultado = false;
+		
+		if(parametros[0].toLowerCase().trim().equals("imagem")){
+			
+			if(parametros.length == 4){
 				
-				criarArquivo(cmd);
+				if(ehInteiro(parametros[1]) && ehInteiro(parametros[2])){
+					if(parametros[3].toLowerCase().trim().equals("g") || parametros[3].toLowerCase().trim().equals("rgb")){
+						resultado = true;
+					}
+				}		
+			}	
+		}
+			
+		if(parametros[0].toLowerCase().trim().equals("salvar")){
+			
+			if(parametros.length == 2) resultado = true;
+			
+			
+		}
+		if(parametros[0].toLowerCase().trim().equals("cor")){
+			
+			if(parametros.length == 4){
+				
+				if(parametros[1].toLowerCase().trim().equals("r") &&
+					parametros[2].toLowerCase().trim().equals("g") &&
+						parametros[3].toLowerCase().trim().equals("b"))	resultado = true;
+				}				
+			
+			if(parametros.length == 2){
+				if(parametros[1].toLowerCase().trim().equals("g")) resultado = true;
+			}
+				
+				
 			}
 			
-			if(cmd.startsWith("cor")){
-				
-				criarCor(cmd);
-			}
+		if((parametros[0].toLowerCase().trim().equals("reta")) || 
+				(parametros[0].toLowerCase().trim().equals("retangulo"))){
 			
-			if(cmd.startsWith("circulo")){
+			for (int i = 1, cont = 0; i < parametros.length; i++) {
 				
-				criarCirculo(cmd);
+				if(ehInteiro(parametros[i])){
+					
+					if((Integer.parseInt(parametros[i]) >= 0 && Integer.parseInt(parametros[i]) <= 255)){
 				
-			}
+					cont++;
+					}
+				}		
+				if(cont == 4) resultado = true;		
+			}	
+		}
+		
+		if(parametros[0].toLowerCase().trim().equals("triangulo")){
 			
-			if(cmd.startsWith("reta")){
-				
-				criarReta(cmd);
-				
-			}
+		}
 			
-			if(cmd.startsWith("retangulo")){
-				
-				criarRetangulo(cmd);
-				
-			}
 			
-			if(cmd.startsWith("triangulo")){
+			for (int i = 1, cont = 0; i < parametros.length; i++) {
 				
-				criarTriangulo(cmd);
-				
-				
-			}
+				if(ehInteiro(parametros[i])){
+					
+					if(	(Integer.parseInt(parametros[i]) >= 0 && Integer.parseInt(parametros[i]) <= 255)){
+						
+						cont++;
+					}
+					if(cont == 6) resultado = true;	
+				}
 			
+		}
+		
+		if(parametros[0].toLowerCase().trim().equals("circulo")){
+			
+			for (int i = 1, cont = 0; i < parametros.length; i++) {
+				
+				if(ehInteiro(parametros[i])){
+					
+					if(	(Integer.parseInt(parametros[i]) >= 0 && Integer.parseInt(parametros[i]) <= 255)){					
+					
+						cont++;
+					}
+				}	
+				
+				if(cont == 3) resultado = true;	
+			}
 			
 			
 		}
 		
-
+		return resultado;
 	}
 
-	private static void criarImagem(String cmd) {
-		
-		String[] parametros = cmd.split(" ");
-		
-		if(parametros.length == 4){
+	private static boolean ehInteiro(String valor) {
+		char[] c = valor.toCharArray();  
+	    boolean flag = true;  
+	  
+	    for ( int i = 0; i < c.length; i++ )  
+	          
+	        if ( !Character.isDigit( c[ i ] ) ) {  
+	            flag = false;  
+	            break;  
+	        }  
+	    
+	    return flag; 
+	}
+
+	private static void executaParametros(String[] parametros) {
+
+		System.out.println(parametros[0]);
+		if(parametros[0].toLowerCase().trim().equals("imagem")){
 			
+			criarImagem(parametros);
+			
+		}
+	
+		if(parametros[0].toLowerCase().trim().equals("salvar")){
+			
+			criarArquivo(parametros);
+		}
+		
+		if(parametros[0].toLowerCase().trim().equals("cor")){
+			
+			criarCor(parametros);
+		}
+		
+		if(parametros[0].toLowerCase().trim().equals("circulo")){
+			
+			criarCirculo(parametros);
+			
+		}
+		
+		if(parametros[0].toLowerCase().trim().equals("reta")){
+			
+			criarReta(parametros);
+			
+		}
+		
+		if(parametros[0].toLowerCase().trim().equals("retangulo")){
+			
+			criarRetangulo(parametros);
+			
+		}
+		
+		if(parametros[0].toLowerCase().trim().equals("triangulo")){
+			
+			criarTriangulo(parametros);
+			
+			
+		}
+		
+		
+	}
+
+	private static void criarImagem(String[] parametros) {
+		
+				
 			System.out.println("funcionou");
-			
-		} else{
-			
-			System.out.println("nao funcionou");
-			
-		}
+	
 		
 	}
 
-	private static void criarArquivo(String cmd) {
+	private static void criarArquivo(String[] parametros) {
 		
-		String[] parametros = cmd.split(" ");
+		System.out.println("funcionou");
+	}
+
+	private static void criarCor(String[] parametros) {
 		
-		if(parametros.length == 2){
-			
-			
-		} else{
-			
-			
-		}
+		System.out.println("funcionou");
 		
 	}
 
-	private static void criarCor(String cmd) {
+	private static void criarCirculo(String[] parametros) {
 		
-		String[] parametros = cmd.split(" ");
-		
-		if(parametros.length == 4){
-			
-			
-		}
-		
-		if(parametros.length == 2){
-			
-		} else{
-			
-			
-		}
-		
-	}
-
-	private static void criarCirculo(String cmd) {
-		
-		String[] parametros = cmd.split(" ");
-		
-		if(parametros.length == 4){
-			
+				
 			Circulo c = new Circulo();
 			System.out.println("funcionou");
-			
-			
-		} else{
-			
-			
-		}
+	
 		
 	}
 
-	private static void criarReta(String cmd) {
-		
-		String[] parametros = cmd.split(" ");
-		
-		if(parametros.length == 5){
-			
+	private static void criarReta(String[] parametros) {
+	
 			Reta r = new Reta();
 			System.out.println("funcionou");
-			
-		} else{
-			
-			
-		}
-		
+
 	}
 
-	private static void criarRetangulo(String cmd) {
+	private static void criarRetangulo(String[] parametros) {
 		
-		String[] parametros = cmd.split(" ");
-		
-		if(parametros.length == 5){
-			
 			Retangulo r = new Retangulo();
 			System.out.println("funcionou");
-			
-		} else{
-			
-			
-		}
-		
 	}
 
-	private static void criarTriangulo(String cmd) {
+	private static void criarTriangulo(String[] parametros) {
 		
-		
-		String[] parametros = cmd.split(" ");
-		
-		if(parametros.length == 4){
 			
 			Ponto[] p = new Ponto[3];
 			
-			for (int i = 0; i < p.length; i++) {
+			/*for (int i = 0; i < p.length; i++) {
 				
 				for (int j = 1; j < parametros.length; j++) {
 					
@@ -180,14 +251,10 @@ public class Principal {
 			
 			Triangulo t = new Triangulo();
 			
-			
+			t.setVertices(p[0], p[1], p[2]);
+			*/
 			System.out.println("funcionou");
-			
-			
-		} else{
-			
-			
-		}
+
 	}
 
 }
--- Trabalho_2/src/Reta.java
+++ Trabalho_2/src/Reta.java
@@ -3,6 +3,42 @@ public class Reta extends Shape{
 	
 	public void setVertices(Ponto p0, Ponto p1){
 		
-	}
+		Imagem img = new Imagem();
+		
+		int dx= Math.abs(p1.x - p0.x);
+		int dy= Math.abs(p1.y - p0.y);
+		
+		int err = dx - dy;
+		int sx, sy;
+		
+		if (p0.x > p1.x) sx = -1;
+		else sx = 1;
+
+		if (p0.y > p1.y) sy = -1;
+		else sy = 1;
+		
+		while (true){
+			
+			
+			//img.setPixel (p0);
+			if (p0.x == p1.x && p0.y == p1.y) break;
+			int e2 = 2 * err;
+
+			if (e2 > - dy) {
+			err = err - dy;
+			p0.x = p0.x + sx;
+			}
+			//img.setPixel (p0);
+			if (p0.x == p1.x && p0.y == p1.y) break;
+			//img.setPixel (); break;
+
+			if (e2 <dx) {
+			err = err + dy;
+			p0.y = p0.y + sy;
+			}
+
+		}
+	
+	}	
 
 }
