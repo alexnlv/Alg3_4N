bc902ee6b9e20afc124fcbeff8c9dcceeab87a1c
alexnlv <alexnlv@hotmail.com>
2013-10-02 22:12:28 -0300

    aula lista

---
+++ aulas/Lista Encadeada/src/ListaEncadeada.java
@@ -0,0 +1,21 @@
+
+public class ListaEncadeada {
+	
+	private Nodo head;
+	private Nodo Tail;
+	
+	public void insert (Nodo before, Nodo novo){
+		
+		
+	}
+	
+	public void remove (Object key){
+		
+	}
+	
+	public Nodo search (Object key){
+		
+	}
+
+	
+}
---
+++ aulas/Lista Encadeada/src/Nodo.java
@@ -0,0 +1,28 @@
+
+public class Nodo {
+	
+	private int conteudo;
+	private Nodo next;
+	
+	public void setNextNodo (Nodo novo){
+		
+	}
+	
+	public Nodo getNext(){
+		
+		
+	}
+	
+	public int getConteudo(){
+		int resultado = 0;
+		
+		
+		return resultado;
+	}
+	
+	public void setConteudo(int valor){
+		
+	}
+
+	
+}
