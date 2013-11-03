0a4561b6af0a7275e38f83fe50fb8c0e5fb5a213
Casa <Casa@Home>
2013-11-03 18:44:46 -0200

    .....

--- BlackTree/src/Arvore.java
+++ BlackTree/src/Arvore.java
@@ -1,52 +1,67 @@
+import java.util.TreeSet;
 
 public class Arvore {
-	
+
 	private String chave;
 	private String valor;
 	private Nodo raiz;
 	private Nodo tio;
-	private Nodo avo;
-	
-	
-	public void insere(String chave, String valor){
-			
-		Nodo N = new Nodo();
-		
-		N.insere(chave.hashCode(), valor);
-		
-		if(raiz.equals(null)){
-			
-			N.setBlack(true);
-			raiz = N;
+	private int altura = 0;
+
+	public void insere(String chave, String valor) {
+
+		Nodo novoNodo = new Nodo(chave.hashCode(), valor);
+
+		if (raiz.equals(null)) {
+
+			novoNodo.black = true;
+			raiz = novoNodo;
+			raiz.altura = 0;
+			raiz.pai = null;
+			raiz.direita = null;
+			raiz.esquerda = null;
 			return;
 		}
-		// Se o hash é maior entao insere na direita
-		if(chave.hashCode()> raiz.getChave()){
+
+		Nodo nodoAux = raiz;
+
+		while (novoNodo.pai.equals(null)) {
+
+			if (novoNodo.indice > nodoAux.indice) {
+				
+				altura++;
+				
+					if (nodoAux.direita.equals(null)) {
+						
+						nodoAux.direita = novoNodo;
+						novoNodo.pai = nodoAux;
+						novoNodo.direita = null;
+						novoNodo.esquerda = null;
+						novoNodo.altura = altura;
+					}
+					
+				nodoAux = nodoAux.direita;	
+			} 
 			
-			raiz.setDir(N);
 			
+
 		}
-		// Se o hash é menor entao insere na esquerda
-		if(chave.hashCode() < raiz.getChave()){
-			
-			raiz.setEsq(N);
+
+			if (novoNodo.indice < nodoAux.indice) {
+
+				nodoAux = nodoAux.esquerda;
+
+				if (nodoAux.equals(null)) {
+
+					novoNodo.pai = nodoAux;
+
+				}
+			}
+
 		}
-		
-		
-		
-		
-		
+
+	public void remover(String chave) {
+
 	}
-	
-	/*public String getValor(String chave){
-	
-		return valor;
-	}*/
-	
-	public void remover(String chave){
-		
-		
-	}
-	
 
 }
--- BlackTree/src/Nodo.java
+++ BlackTree/src/Nodo.java
@@ -1,86 +1,16 @@
 
 public class Nodo {
 	
-	private String conteudo;
-	private int indice;
-    private Nodo direita;
-    private Nodo esquerda;    
-    private Nodo pai;
-    private boolean black = true;
+	public String conteudo;
+	public int indice, altura;
+	public Nodo direita, esquerda, pai;
+	public boolean black = true;
 	
-	//setters e getters 
-    public void insere(int indice, String valor){
-    	
-    	this.indice = indice;
+	public Nodo(int indice, String valor) {
+		
+		this.indice = indice;
     	this.conteudo = valor;
-    	
-    }
-    
-    public void setBlack(boolean flag){
-    	
-    	this.black = flag;
-    	
-    }
-    
-    public void setDir(Nodo dir){
-		
-    	this.direita = dir;
-    	
-	}
-	
-	public void setEsq(Nodo esq){
-				
-		this.esquerda = esq;
-	}
-	
-	public void setPai(Nodo pai){
-		
-		this.pai = pai;
-		
-	}
-	
-	public int getChave(){
-		
-		return indice;
-	}
-	
-	
-	public Nodo getNodoEsq(){
-		
-		return esquerda;
-	}
-	
-	public Nodo getNodoDir(){
-		
-		return direita;
-	}
-	
-	public Nodo getNodoPai(){
-		
-		return pai;
-	}
-	
-	public boolean ehBlack(){
-		
-		return black;
-	}
-	
-	public int altura(){
-		
-		int resultado = 0;
-		
-		
-		return resultado;
-	}
-	
-	
-	public int balanca(){
-		
-		int resultado = 0;
-			
-		return resultado;
-		
-	}
-	
 
+	}
+    
 }
